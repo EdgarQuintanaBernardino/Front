1955f87f32c58c9311d0db94126f7e0edf963bce
edgarquintanabernardino1989@gmail.com <edgarquintanabernardino1989@gmail.com>
2021-03-10 21:53:01 -0600

    cambios desde casa

--- src/views/tablesdetails/empresashow.vue
+++ src/views/tablesdetails/empresashow.vue
@@ -67,7 +67,6 @@ export default {
                           email:"",
                           roles:"",
                           nickname:"",
-                          actions:""
                         }
     
      
